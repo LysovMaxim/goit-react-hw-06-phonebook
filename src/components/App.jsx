0a69505c7fa58b80d27bd1414545cecdc9a36528
LysovMaxim <max_maran@icloud.com>
2023-06-11 01:18:07 +0300

    Переіменував папку на redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { Forma } from './Forma';
 import { nanoid } from 'nanoid';
 import { ContainerForm } from './App.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact, deleteContacts } from 'store/sliceContact';
-import { addValue } from 'store/sliceFilter';
+import { addContact, deleteContacts } from 'redux/sliceContact';
+import { addValue } from 'redux/sliceFilter';
 
 export const App = () => {
   const contactsValue = useSelector(state => state.contacts);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'store/store';
+import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
